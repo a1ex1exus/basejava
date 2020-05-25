6f6ea382e03b0658d5ed645bdc006c4c77f305eb
Oleg Makarov <oleh.makarov@gmx.de>
2020-05-25 09:45:11 +0200

    HomeWork1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int nElems = 0;
+    int size = 0;
 
     void clear() {
-        Arrays.fill(Arrays.copyOf(storage, nElems), null);
-        nElems = 0;
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
         System.out.println("The array has been cleared");
     }
 
     void save(Resume r) {
-        if (nElems <= storage.length - 1) {
-            storage[nElems] = r;
-            nElems++;
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
         } else {
             System.out.println("Not enough space to insert a new resume." +
                     "\n First delete an unnecessary resume and try again.");
@@ -25,12 +25,12 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         int i;
-        for (i = 0; i < nElems; i++) {
+        for (i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                break;
+                return storage[i];
             }
         }
-        if (i == nElems) {
+        if (i == size) {
             System.out.println("Resume not found");
             return null;
         } else {
@@ -40,20 +40,20 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int i;
-        for (i = 0; i < nElems; i++) {
+        for (i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 break;
             }
         }
-        if (i == nElems) {
+        if (i == size) {
             System.out.println("The resume " + uuid + " not found. \n" +
                     "Unable to delete a resume that does not exist");
         } else {
-            for (int k = i; k < nElems-1; k++) {
+            for (int k = i; k < size - 1; k++) {
                 storage[k] = storage[k + 1];
             }
-            storage[nElems-1] = null;
-            nElems--;
+            storage[size - 1] = null;
+            size--;
             System.out.println("The resume " + uuid + "  has been found and deleted.");
         }
     }
@@ -62,10 +62,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, nElems);
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return nElems;
+        return size;
     }
 }
