43715ef1db44405a43a12c4cdb98b7b1fc4294b0
Oleg Makarov <oleh.makarov@gmx.de>
2020-05-27 06:58:55 +0200

    HomeWork1_Update1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -24,33 +24,30 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        int i;
-        for (i = 0; i < size; i++) {
+
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
-        if (i == size) {
-            System.out.println("Resume not found");
-            return null;
-        } else {
-            return storage[i];
-        }
+        System.out.println("No resume found.");
+        return null;
     }
 
     void delete(String uuid) {
-        int i;
+        int i, index = -1;
         for (i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
+                index = i;
                 break;
             }
         }
-        if (i == size) {
+        if (index == -1) {
             System.out.println("The resume " + uuid + " not found. \n" +
                     "Unable to delete a resume that does not exist");
         } else {
-            for (int k = i; k < size - 1; k++) {
-                storage[k] = storage[k + 1];
+            for (int j = i; j < size - 1; j++) {
+                storage[j] = storage[j + 1];
             }
             storage[size - 1] = null;
             size--;
